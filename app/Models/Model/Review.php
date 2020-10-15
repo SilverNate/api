88b462c855c2667b6e139577746e6476c62c0e82
andean <andean@thefthing.com>
2020-10-16 01:19:51 +0700

    relationship created models

--- app/Models/Model/Review.php
+++ app/Models/Model/Review.php
@@ -4,8 +4,14 @@ namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use  \App\Models\Model\Product;
+
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
